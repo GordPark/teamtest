9b25a512f4ab991a5511b89c26b9bccceb5b4b26
GordPark <gord23p@gmail.com>
2024-03-11 14:54:34 +0900

    사칙연산

--- divide.py
+++ divide.py
@@ -1,3 +1,7 @@
-def divide(a,b):
+# 나누기 함수
+
+def divide():
+    a = int(input("첫번째 숫자 입력: "))
+    b = int(input("첫번째 숫자 입력: "))
     return a / b
     
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # 팀 브런치 사칙연산
 from divide import divide
-a = 4
-b = 2
+
 divide()
